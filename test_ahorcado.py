d96e0f06b07e401398387a0d07173eb0ce1c561f
Camila Salvia <camilasalvia2@gmail.com>
2025-09-08 21:50:53 -0300

    codigo "validar si la letra pertenece"

--- test_ahorcado.py
+++ test_ahorcado.py
@@ -1,9 +1,8 @@
-from ahorcado import validar_letra
+from ahorcado import letra_pertenece_palabra, palabra_secreta
 
 # test 1: valida letra y acierta
 def test_validar_letra_acierto():
   assert letra_pertenece_palabra("a", palabra_secreta) is True
-
 
 #test 2: valida letra y falla
 def test_validar_letra_falla():
@@ -11,8 +10,8 @@ def test_validar_letra_falla():
 
 # test 3: la letra vale, así sea mayúscula o minúscula
 def test_validar_mayusc_minusc():
-	assert letra_pertenece_palabra("a", palabra_secreta) is True
-  assert letra_pertenece_palabra("A", palabra_secreta) is True
+    assert letra_pertenece_palabra("a", palabra_secreta) is True
+    assert letra_pertenece_palabra("A", palabra_secreta) is True
 
 # test 4: ingreso de caracter inválido
 def test_validar_caracter_invalido():
