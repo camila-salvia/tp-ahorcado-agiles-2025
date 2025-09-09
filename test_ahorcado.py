d87bbb633d0cc2d6a0cd67b4c24c9109da15bf83
marileonardelli <marileonardelli@hotmail.com>
2025-09-08 21:31:08 -0300

    test_valida_letra_falla

--- test_ahorcado.py
+++ test_ahorcado.py
@@ -3,3 +3,8 @@ from ahorcado import validar_letra
 # test 1: valida letra y acierta
 def test_validar_letra_acierto():
   assert letra_pertenece_palabra("a", palabra_secreta) is True
+
+
+#test 2: valida letra y falla
+def test_validar_letra_falla():
+  assert letra_pertenece_palabra("z", palabra_secreta)
