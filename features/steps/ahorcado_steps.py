c16630880de2b4eb8b8f3ac83e9688d80d0719e6
Camila <129900822+camila-salvia@users.noreply.github.com>
2025-11-10 20:54:14 -0300

    Configure Selenium WebDriver for headless mode
    Add Chrome options for headless execution in tests

--- features/steps/ahorcado_steps.py
+++ features/steps/ahorcado_steps.py
@@ -1,6 +1,13 @@
 from behave import given, when, then
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 import time
 import os
 
