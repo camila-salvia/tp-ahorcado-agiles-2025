b58e6ad53ca1a19971ae64749d4762da54f9c7cc
Camila Salvia <camilasalvia2@gmail.com>
2025-12-09 09:27:04 -0300

    arreglo problema de seguridad cors

--- ahorcado.py
+++ ahorcado.py
@@ -3,8 +3,10 @@ import os
 from palabras import palabras_posibles
 from flask import Flask, request, jsonify
 from flask_wtf.csrf import CSRFProtect
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*", "send_wildcard": "False"}})
 csrf = CSRFProtect()
 csrf.init_app(app)
 
